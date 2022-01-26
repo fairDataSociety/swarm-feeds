671a00ef93cc7c1446af2d44c6fc001455907892
nugaon <toth.viktor.levente@gmail.com>
2022-01-26 11:33:24 +0100

    fix: test setup waiting for postage batch polling

--- test/tests-setup.ts
+++ test/tests-setup.ts
@@ -1,19 +1,26 @@
 /* eslint-disable no-console */
-import { BeeDebug } from '@ethersphere/bee-js'
+import { BeeDebug, DebugPostageBatch } from '@ethersphere/bee-js'
+
+async function sleep(ms = 1000): Promise<void> {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
 
 export default async function testsSetup(): Promise<void> {
   if (!process.env.BEE_POSTAGE) {
     try {
       console.log('Creating postage stamps...')
       const beeDebugUrl = process.env.BEE_DEBUG_API_URL || 'http://localhost:1635'
-      const bee = new BeeDebug(beeDebugUrl)
-      process.env.BEE_POSTAGE = await bee.createPostageBatch('1', 20)
+      const beeDebug = new BeeDebug(beeDebugUrl)
+      process.env.BEE_POSTAGE = await beeDebug.createPostageBatch('1', 20)
       console.log('BEE_POSTAGE: ', process.env.BEE_POSTAGE)
-      // sleep for 11 seconds (10 blocks with ganache block time = 1s)
-      // needed for postage batches to become usable
-      // FIXME: sleep should be imported for this, but then we fail with
-      //        Could not find a declaration file for module 'tar-js'
-      await new Promise<void>(resolve => setTimeout(() => resolve(), 11_000))
+      //wait for chunk to be usable
+      let postageBatch: DebugPostageBatch
+      do {
+        postageBatch = await beeDebug.getPostageBatch(process.env.BEE_POSTAGE)
+
+        console.log('Waiting 1 sec for batch ID settlement...')
+        await sleep()
+      } while (!postageBatch.usable)
     } catch (e) {
       // It is possible that for unit tests the Bee nodes does not run
       // so we are only logging errors and not leaving them to propagate
